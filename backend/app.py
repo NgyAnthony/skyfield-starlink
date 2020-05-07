ef8243b28fafabd880c9a3c3e00794efe1e85396
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-05-07 14:59:48 +0200

    Remove CORS

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask import jsonify
-from flask_cors import CORS, cross_origin
 from skyfield.api import load
 
 app = Flask(__name__)
-CORS(app)
 
 
 @app.route('/')
