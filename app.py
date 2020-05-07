05c3cc7ebfec812535fbbf59602fc766bff3c25a
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-05-07 15:56:50 +0200

    Add CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, url_for
 from flask import jsonify
 from skyfield.api import load
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def fetch_satellites():
